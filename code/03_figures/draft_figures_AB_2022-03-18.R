0c80fcb82dd4fb241c97183a20111d9617e09b8b
an-bui <a.thanh.bui@gmail.com>
2022-03-31 09:52:26 -0700

    some figure edits

--- code/03_figures/draft_figures_AB_2022-03-18.R
+++ code/03_figures/draft_figures_AB_2022-03-18.R
@@ -14,7 +14,9 @@ source(here("code",
             "02_analyses",
             "isotope_niches.R"))
 
-
+# store colors
+cn_col <- "#bf812d"
+pg_col <- "#80cdc1"
 
 # Figure 1: Top pred niche stuff ------------------------------------------
 
@@ -60,8 +62,8 @@ iso_niche_opt2 <- spider_iso %>%
                                 "#80cdc1")) +
   stat_ellipse(size = 1) +
   # in plot annotations of CN and PG
-  annotate("text", x = -26, y = 7.2, label = "CN", size = 12, col = "#80cdc1") +
-  annotate("text", x = -20, y = 3.5, label = "PG", size = 12, col = "#bf812d") +
+  annotate("text", x = -26, y = 7.2, label = "Cocos", size = 11, col = cn_col) +
+  annotate("text", x = -20.1, y = 3.4, label = "Pisonia", size = 11, col = pg_col) +
   theme_bw() +
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 14),
@@ -69,6 +71,41 @@ iso_niche_opt2 <- spider_iso %>%
         plot.margin = margin(1, 1, 0.5, 0.5, unit = "cm"))
 
 iso_niche_opt2
+
+iso_niche_opt3 <- spider_iso %>%
+  mutate(Island = factor(Island, 
+                         levels = c("Castor", "Fern",
+                                    "Holei","Kaula", "Paradise", 
+                                    "Dudley", "Eastern",
+                                    "Leslie", "Lost", "Sand"))) %>%
+  ggplot(aes(x = d13C, y = d15N_c, color = Island)) +
+  geom_point() +
+  labs(x = expression({delta}^13*C~ ('\u2030')), 
+       y = expression({delta}^15*N~ ('\u2030'))) +
+  scale_color_manual(values = c("#2D1A03", # Castor
+                                "#543005", # Fern
+                                "#8c510a", # Holei
+                                "#bf812d", # Kaula
+                                "#dfc27d", # Paradise
+                                "#c7eae5", # Dudley
+                                "#80cdc1", # Eastern
+                                "#35978f", # Leslie
+                                "#01665e", # Lost
+                                "#003c30" # Sand
+                                )) +
+  stat_ellipse(aes(linetype = Habitat)) +
+  scale_linetype_manual(values = c(1, 2), guide = "none") +
+  # in plot annotations of CN and PG
+  annotate("text", x = -26.7, y = 7.4, label = "Pisonia", size = 10.5, col = pg_col) +
+  annotate("text", x = -20.05, y = 3.4, label = "Cocos", size = 10.5, col = cn_col) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 12)
+  ) 
+
+iso_niche_opt3
 
 # Figure1b: DNA niche -----------------------------------------------------
 
@@ -87,11 +124,27 @@ top_comm <- xt %>%
   theme_bw() +
   labs(title = "Top predators")
 
+top_comm_opt2 <- xt %>%
+  rownames_to_column(var = "Extraction.ID") %>%
+  left_join(DNA_meta, by = "Extraction.ID") %>%
+  ggplot(aes(x = CAP1, y = MDS1, color = Habitat)) +
+  geom_point(size = 2) +
+  scale_fill_manual(values = c(cn_col, pg_col)) +
+  scale_color_manual(values = c(cn_col, pg_col)) +
+  stat_ellipse(size = 1) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 12),
+        legend.position = "none"
+  ) +
+  # in plot annotations of CN and PG
+  annotate("text", x = -4, y = 1.5, label = "Cocos", size = 11, col = cn_col) +
+  annotate("text", x = 3, y = 2, label = "Pisonia", size = 11, col = pg_col) +
+  labs(title = "Top predators")
 
-# Question 2: Do DNA diet trophic niches of top predators shift across environmental contexts?
-# the difference in number of dots here is kinda tripping me up - is there any way to filter the 
-# isotope data to only include the ones with DNA data to match? I think the most recent draft that
-# I have doesn't include the isotope methods so I'm sorry if this is totally unreasonable!!
+top_comm_opt2
 
 # Fig1c: isotopic niche box plot -------------------------------------------
 
@@ -109,14 +162,21 @@ niche_box <- ggplot(all_niche, aes(x = Method, y = ShapeArea, fill = Habitat)) +
 # just another option - I think it's worth seeing the distribution of data somehow, whether it's
 # a boxplot with jittered points on top, or a violin?
 
-niche_box_opt2 <- ggplot(all_niche, aes(x = Method, y = ShapeArea, fill = Habitat)) +
+niche_box_opt2 <- all_niche %>% 
+  mutate(Habitat = recode(Habitat, CN = "Cocos", PG = "Pisonia")) %>% 
+  ggplot(aes(x = Method, y = ShapeArea, fill = Habitat)) +
   geom_violin() +
   labs(y = "95% isotopic niche area", 
        x = "Isotopic niche method",
        fill = "Habitat") +
-  scale_fill_manual(values = c("#bf812d",
-                               "#80cdc1")) +
-  theme_bw()
+  scale_fill_manual(values = c(cn_col,
+                               pg_col)) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 12)
+  ) 
 
 niche_box_opt2
 
@@ -139,22 +199,37 @@ top_id <- ggplot(islet_prey, aes(x = Habitat, y = Frequency, fill = Order)) +
 
 # Very hacky solution...
 
+# old colors
+# Order == "Araneae" ~ "#88cfe3",
+# Order == "Blattodea" ~ "#43c2e6",
+# Order == "Dermaptera" ~ "#0facd9",
+# Order == "Diptera" ~ "#468294",
+# Order == "Orthoptera" ~ "#0d5f78"
+
 # create a new data frame
 df_prey <- islet_prey %>% 
   pivot_wider(names_from = Habitat, values_from = Frequency) %>% 
   # shades of blue for shared orders
   mutate(color_col = case_when(
-    Order == "Araneae" ~ "#88cfe3",
-    Order == "Blattodea" ~ "#43c2e6",
-    Order == "Dermaptera" ~ "#0facd9",
-    Order == "Diptera" ~ "#468294",
-    Order == "Orthoptera" ~ "#0d5f78",
+    Order == "Araneae" ~ "#A1CAF6",
+    Order == "Blattodea" ~ "#6592D6",
+    Order == "Dermaptera" ~ "#4C6FA1",
+    Order == "Diptera" ~ "#375377",
+    Order == "Orthoptera" ~ "#1E2F46",
     # use grey for the orders that are not shared
-    TRUE ~ "#8e9191"
+    Order == "Hemiptera" ~ "#E0E0E0",
+    Order == "Lepidoptera" ~ "#CBCBCB",
+    Order == "Geophilomorpha" ~ "#B7B7B7",
+    Order == "Hymenoptera" ~ "#A2A2A2",
+    Order == "Odonata" ~ "#8D8D8D",
+    Order == "Psocoptera" ~ "#787878",
+    Order == "Sarcoptiformes" ~ "#646464",
+    Order == "Scorpiones" ~ "#4F4F4F"
   )) %>% 
   pivot_longer(CN:PG, names_to = "Habitat", values_to = "Frequency") %>% 
   ungroup() %>% 
-  fill(Order)
+  fill(Order) %>% 
+  mutate(Habitat = recode(Habitat, CN = "Cocos", PG = "Pisonia"))
 
 levels_prey <- c(
   # five shared species in alphabetical order
@@ -169,7 +244,7 @@ levels_prey <- c(
 
 # CN data frame
 cn_prey <- df_prey %>% 
-  filter(Habitat == "CN") %>% 
+  filter(Habitat == "Cocos") %>% 
   mutate(Order = fct_relevel(Order, levels_prey)) %>% 
   arrange(Order) 
 
@@ -177,7 +252,7 @@ cn_prey_col <- pull(cn_prey, color_col)
 
 cn_prey_plot <- ggplot(cn_prey, aes(x = Habitat, y = Frequency, fill = Order)) +
   geom_col(color = "black", position = "fill") +
-  scale_fill_manual(values = cn_prey_col) +
+  scale_fill_manual(values = cn_prey_col, drop = FALSE) +
   theme_bw() +
   theme(legend.position = "none",
         axis.title.x = element_blank(),
@@ -193,7 +268,7 @@ cn_prey_plot <- ggplot(cn_prey, aes(x = Habitat, y = Frequency, fill = Order)) +
   facet_wrap(~Habitat, strip.position = "bottom")
 
 pg_prey <- df_prey %>% 
-  filter(Habitat == "PG") %>% 
+  filter(Habitat == "Pisonia") %>% 
   mutate(Order = fct_relevel(Order, levels_prey)) %>% 
   arrange(Order)
 
@@ -216,17 +291,17 @@ pg_prey_plot <- ggplot(pg_prey, aes(x = Habitat, y = Frequency, fill = Order)) +
         ) +
   facet_wrap(~Habitat, strip.position = "bottom")
 
-library(patchwork) # putting things together
+top_id_opt2 <- cn_prey_plot + pg_prey_plot
 
-together <- cn_prey_plot + pg_prey_plot
-
-together
+top_id_opt2
 
 # Fig1: Put together and save ---------------------------------------------
 
 
 fig1 <- (iso_niche + top_comm)/(niche_box + top_id)
 fig1
+
+fig1_opt2 <- (iso_niche_opt3 + top_comm_opt2)/(niche_box_opt2 + top_id_opt2)
 
 ggsave(plot = fig1,
        filename = 'top_niche.png',
@@ -259,6 +334,28 @@ int_comm <- xi %>%
   theme_bw() +
   labs(title= "Intermediate predators")
 
+int_comm_opt2 <- xi %>%
+  rownames_to_column(var = "Extraction.ID") %>%
+  left_join(DNA_intmeta, by = "Extraction.ID") %>%
+  ggplot(aes(x = CAP1, y = MDS1, color = Habitat)) +
+  geom_point(size = 2) +
+  scale_fill_manual(values = c(cn_col, pg_col)) +
+  scale_color_manual(values = c(cn_col, pg_col)) +
+  stat_ellipse(size = 1) +
+  theme_bw() +
+  theme(axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text = element_text(size = 11),
+        legend.title = element_text(size = 12),
+        legend.position = "none"
+  ) +
+  # in plot annotations of CN and PG
+  annotate("text", x = -2, y = -1.5, label = "Cocos", size = 11, col = cn_col) +
+  annotate("text", x = 1, y = 2, label = "Pisonia", size = 11, col = pg_col) +
+  labs(title = "Intermediate predators")
+
+int_comm_opt2
+
 
 # Fig2b: Secondary Pred prey composition ---------------------------------------
 
@@ -274,24 +371,35 @@ int_id <- ggplot(habitat_int, aes(x = Habitat, y = Frequency, fill = Order)) +
 
 # again, super hacky solution...
 
+# old colors: 
+# Order == "Araneae" ~ "#deb962",
+# Order == "Diptera" ~ "#d4bf8a",
+# Order == "Hymenoptera" ~ "#bf8c11",
+# Order == "Lepidoptera" ~ "#967f45",
+# Order == "Orthoptera" ~ "#876d2a", 
+# Order == "Psocoptera" ~ "#6b5210"
+
 # create a new data frame
 df_int <- habitat_int %>% 
   pivot_wider(names_from = Habitat, values_from = Frequency) %>% 
   # shades of blue for shared orders
   mutate(color_col = case_when(
-    # yellowish browns for shared orders
-    Order == "Araneae" ~ "#deb962",
-    Order == "Diptera" ~ "#d4bf8a",
-    Order == "Hymenoptera" ~ "#bf8c11",
-    Order == "Lepidoptera" ~ "#967f45",
-    Order == "Orthoptera" ~ "#876d2a", 
-    Order == "Psocoptera" ~ "#6b5210", 
+    # yellowish oranges for shared orders
+    Order == "Araneae" ~ "#F2B705",
+    Order == "Diptera" ~ "#F29F05",
+    Order == "Hymenoptera" ~ "#F28705",
+    Order == "Lepidoptera" ~ "#D95204",
+    Order == "Orthoptera" ~ "#A62F03", 
+    Order == "Psocoptera" ~ "#701e00", 
     # use grey for the orders that are not shared
-    TRUE ~ "#8e9191"
+    Order == "Blattodea" ~ "#E0E0E0",
+    Order == "Coleoptera" ~ "#787878",
+    Order == "Thysanoptera" ~ "#B7B7B7"
   )) %>% 
   pivot_longer(CN:PG, names_to = "Habitat", values_to = "Frequency") %>% 
   ungroup() %>% 
-  fill(Order)
+  fill(Order) %>% 
+  mutate(Habitat = recode(Habitat, CN = "Cocos", PG = "Pisonia"))
 
 levels_pred <- c(
   # six shared species in alphabetical order
@@ -299,12 +407,12 @@ levels_pred <- c(
   "Lepidoptera", "Orthoptera", "Psocoptera",
   
   # unique species
-  "Blattodea", "Coleoptera", "Thysanoptera"
+  "Blattodea", "Thysanoptera", "Coleoptera"
 )
 
 # CN data frame
 cn_int <- df_int %>% 
-  filter(Habitat == "CN") %>% 
+  filter(Habitat == "Cocos") %>% 
   arrange(-Frequency) %>% 
   mutate(Order = fct_relevel(Order, levels_pred)) %>% 
   arrange(Order) 
@@ -329,7 +437,7 @@ cn_int_plot <- ggplot(cn_int, aes(x = Habitat, y = Frequency, fill = Order)) +
   facet_wrap(~Habitat, strip.position = "bottom")
 
 pg_int <- df_int %>% 
-  filter(Habitat == "PG") %>% 
+  filter(Habitat == "Pisonia") %>% 
   mutate(Order = fct_relevel(Order, levels_pred)) %>% 
   arrange(Order)
 
@@ -352,9 +460,9 @@ pg_int_plot <- ggplot(pg_int, aes(x = Habitat, y = Frequency, fill = Order)) +
   ) +
   facet_wrap(~Habitat, strip.position = "bottom")
 
-together <- cn_int_plot + pg_int_plot
+int_id_opt2 <- cn_int_plot + pg_int_plot
 
-together
+int_id_opt2 
 
 
 # Figure2: Put it together and output -------------------------------------
@@ -362,6 +470,11 @@ together
 
 fig2 <- int_comm + int_id
 fig2
+
+fig2_opt2 <- int_comm_opt2 + int_id_opt2 +
+  plot_layout(widths = c(3, 3))
+fig2_opt2
+
 ggsave(plot = fig2,
        filename = 'int_niche.png',
        path = here("pictures", "R"),
